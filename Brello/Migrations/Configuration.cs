22ccd3f83a245b6c5815cdbc92101440ca682330
Kate <katerebekah@gmail.com>
2015-12-05 10:27:40 -0600

    more failed attempts to figure out async await

---
+++ Brello/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace Brello.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Brello.Models.BoardContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Brello.Models.BoardContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+            context.Colors.AddOrUpdate(
+                
+                );
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
